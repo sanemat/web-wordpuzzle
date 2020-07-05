40afaa0fa8e3b1aab4a77725aad00de16feb2b69
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-07-05 16:46:44 +0700

    feat(dictionary): skelton

--- src/script.js
+++ src/script.js
@@ -48,6 +48,10 @@ let store;
 /** @type {Debug} debug */
 let debug;
 
+/** @typedef {{Object: any}} Words */
+/** @type {Words} words */
+let words;
+
 /**
  * @returns {Store}
  */
@@ -623,6 +627,16 @@ async function nextAction() {
   return render();
 }
 
+/**
+ * @promise
+ * @reject {Error}
+ * @fulfill {Boolean}
+ * @returns {Promise.<Boolean>}
+ */
+function loadDictionary() {
+  return Promise.resolve(true);
+}
+
 (() => {
   if (typeof window === "undefined") {
     return;
@@ -636,6 +650,14 @@ async function nextAction() {
       console.error(err);
     }
   });
+  window.addEventListener("load", async () => {
+    try {
+      await loadDictionary();
+    } catch (/** @type {Error|string} */ err) {
+      console.error(err);
+    }
+  });
+
   const play = document.body.querySelector(".js-play");
   if (play) {
     play.addEventListener("submit", playAction);
